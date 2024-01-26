ba3995e8c77c03bdf2926a9b3b5995559cb330e8
LjMedinaBPSU <leoneilmedina01252004@gmail.com>
2024-01-26 18:57:33 +0800

    Sourcecode Upload

---
+++ src/caseStudy/Variables.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caseStudy;
+
+/**
+ *
+ * @author User01
+ */
+public class Variables {
+    String username = " ";
+    
+    public void setusername(String username){
+        this.username = username;
+    }
+    public String getusername(){
+        return username;
+    }
+}
